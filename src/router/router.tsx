5935e0fa5efe1bc4b6ef4b1c295b6c1ce675ca71
vedmax823 <107627040+vedmax823@users.noreply.github.com>
2023-12-25 22:17:04 +0000

    delete

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainPage from "../pages/mainPage";
 import ShipsPage from "../pages/ships/shipsPage";
-import { DRAGONS_ROUTE, DRAGON_ONE_ROUTE, ERROR_ROUTE, MAIN_ROUTE, SHIPS_ROUTE, SHIP_ONE_ROUTE } from "../utils/constants";
+
 import DragonsPage from "../pages/dragons/dragonsPage";
 import OneDragonPage from "../pages/dragons/oneDragonPage";
 import OneShipPage from "../pages/ships/oneShipPage";
 import Error from "../pages/errorPage";
+import { DRAGONS_ROUTE, DRAGON_ONE_ROUTE, ERROR_ROUTE, MAIN_ROUTE, SHIPS_ROUTE, SHIP_ONE_ROUTE } from "../utils/constantsRouts";
 const router = createBrowserRouter([
     {
       path: MAIN_ROUTE,
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -91,11 +91,6 @@ export const limits = [
     }
 ]
 
-export const MAIN_ROUTE = "/";
-export const SHIPS_ROUTE = "/ships";
-export const DRAGONS_ROUTE = "/dragons";
-export const DRAGON_ONE_ROUTE = "/dragons/:dragonId";
-export const SHIP_ONE_ROUTE = "/ships/:shipId";
-export const ERROR_ROUTE = "*"
+
 
 
